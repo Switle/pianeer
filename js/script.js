46b50b027bdf45b936223e67dccf99529cf6d553
Switle <69729725+Switle@users.noreply.github.com>
2023-09-06 19:30:26 +0300

    На обновленной сборке
    Обновил свою сборку галпа и заново сверстал на ней этот лендинг. Еще форму добавил.

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function burger() {
+	const menu = document.querySelector('.menu');
+	const burger = document.querySelector('.burger');
+
+	burger.addEventListener('click', () => {
+		menu.classList.toggle('active');
+		burger.classList.toggle('active');
+	});
+}
+
+burger();
+
